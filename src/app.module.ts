4d7719843eb02dad2cba5bfe8bcc84c2b9f5e2a7
kereminci <kereminci52@gmail.com>
2023-05-02 21:56:18 +0300

    port fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,8 @@ import { EthTransaction } from './eth-transactions/entities/eth-transaction.enti
 @Module({
   imports: [EthTransactionsModule, IpfsModule, TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'host.docker.internal',
-    port: 4306,
+    host: 'localhost',
+    port: 3306,
     username: 'user123',
     password: 'root',
     database: 'db',
