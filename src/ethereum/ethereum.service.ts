ce71f1c6ee198b5ec5c6598b492a25d620018b48
Halil Uzer <haliluzer@outlook.com>
2023-05-28 20:20:59 +0300

    ethereum update

--- src/ethereum/ethereum.service.ts
+++ src/ethereum/ethereum.service.ts
@@ -7,7 +7,6 @@ export class EthereumService {
 
     constructor(@Inject('WEB3') private readonly web3: Web3,
     @Inject() private ethereumAccountRepository: EthereumAccountRepository){}
-
     async createAccount(userId: string){
     const account = this.web3.eth.accounts.create();
     await this.ethereumAccountRepository.saveAccount(userId, account);
